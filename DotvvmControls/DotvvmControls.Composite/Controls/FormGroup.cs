c0d0500b6f73058a30d91b77af96eab65f9ba9c6
Tomáš Herceg <tomas.herceg@riganti.cz>
2022-02-05 18:24:38 +0100

    Second demo added

---
+++ DotvvmControls/DotvvmControls.Composite/Controls/FormGroup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using DotVVM.Framework.Controls;
+
+namespace DotvvmControls.Composite.Controls;
+
+public class FormGroup : CompositeControl
+{
+
+    public static DotvvmControl GetContents(
+        string labelText,
+        params DotvvmControl[] content
+    )
+    {
+        return new HtmlGenericControl("div")
+            .AddCssClass("mb-3")
+            .AppendChildren(
+                new HtmlGenericControl("label")
+                    .AddCssClass("form-label")
+                    .SetProperty(l => l.InnerText, labelText)
+            )
+            .AppendChildren(content);
+    }
+
+}
---
+++ DotvvmControls/DotvvmControls.Composite/Controls/StringField.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using DotVVM.Framework.Binding;
+using DotVVM.Framework.Binding.Expressions;
+using DotVVM.Framework.Controls;
+
+namespace DotvvmControls.Composite.Controls;
+
+public class StringField : CompositeControl 
+{
+
+    public static DotvvmControl GetContents(
+        IValueBinding<string> text,
+        string labelText,
+        [DefaultValue(true)] ValueOrBinding<bool> enabled
+    )
+    {
+        return FormGroup.GetContents(
+            labelText, 
+            new TextBox()
+                .AddCssClass("form-control")
+                .SetProperty(t => t.Text, text)
+                .SetProperty(t => t.Enabled, enabled)
+                .SetAttribute("readonly", enabled.Negate())
+                .SetProperty(Validator.ValueProperty, text)
+        );
+    }
+}
